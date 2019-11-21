343a4f22f8e85cf6c25a725393bf419ee0eb9e1f
Grigori Prokhorov <hello@feinberg.it>
2019-11-21 13:05:58 +0100

    add basic extension definition files

---
+++ ext_emconf.php
@@ -0,0 +1,32 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'Page references',
+    'description' => 'Allows to create references of whole pages, including page properties',
+    'category' => '',
+    'author' => 'Grigori Prokhorov',
+    'author_email' => 'hello@feinberg.it',
+    'shy' => '',
+    'dependencies' => '',
+    'conflicts' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'alpha',
+    'internal' => '',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 0,
+    'lockType' => '',
+    'author_company' => 'feinberg.it',
+    'version' => '0.0.1',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '9.5.11-9.5.99',
+        ),
+        'conflicts' => array(),
+        'suggests' => array(),
+    ),
+    '_md5_values_when_last_written' => 1,
+    'suggests' => array(),
+);
