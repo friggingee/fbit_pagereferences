44a43fe40798d4ccd53a2be17d0e7711ce4c14e1
Grigori Prokhorov <hello@feinberg.it>
2020-04-03 16:54:46 +0200

    Release 1.3.6: Improve output of reference pages list on reference source page.

--- Classes/Hooks/Backend/Controller/PageLayoutController/DrawHeaderHook/PagePropertiesReferencedFlashMessage.php
+++ Classes/Hooks/Backend/Controller/PageLayoutController/DrawHeaderHook/PagePropertiesReferencedFlashMessage.php
@@ -6,9 +6,16 @@ use FBIT\PageReferences\Utility\ReferencesUtility;
 use TYPO3\CMS\Backend\Controller\PageLayoutController;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\Messaging\AbstractMessage;
+use TYPO3\CMS\Core\Site\SiteFinder;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class PagePropertiesReferencedFlashMessage
 {
+    /**
+     * @param array $params
+     * @param PageLayoutController $pageLayoutController
+     * @throws \TYPO3\CMS\Core\Exception\SiteNotFoundException
+     */
     public function addPagePropertiesReferencedFlashMessage(array &$params, PageLayoutController &$pageLayoutController)
     {
         $pageRecord = BackendUtility::getRecord('pages', $pageLayoutController->id);
@@ -24,24 +31,39 @@ class PagePropertiesReferencedFlashMessage
 
         // If properties of a regular page is referenced somewhere else:
         if (ReferencesUtility::hasReferences($pageLayoutController->id)) {
+            $siteFinder = GeneralUtility::makeInstance(SiteFinder::class);
+
             $propertiesUsedOn = [];
 
             foreach (ReferencesUtility::getReferences($pageLayoutController->id) as $referencePage) {
                 $referencePageData = BackendUtility::getRecord('pages', $referencePage['uid']);
 
                 if ($referencePageData['tx_fbit_pagereferences_reference_page_properties']) {
-                    $propertiesUsedOn[] = $referencePageData;
+                    $rootPageId = $siteFinder->getSiteByPageId($referencePage['uid'])->getRootPageId();
+
+                    $propertiesUsedOn[$rootPageId]['rootPageData'] = BackendUtility::getRecord(
+                        'pages',
+                        $rootPageId
+                    );
+
+                    $propertiesUsedOn[$rootPageId]['pages'][] = $referencePageData;
                 }
             }
 
             if (!empty($propertiesUsedOn)) {
-                $messageBodyData = [];
+                $messageBody = '';
 
-                foreach ($propertiesUsedOn as $page) {
-                    $messageBodyData[] = $page['title'] . ' (PID ' . $page['uid'] . ')';
+                foreach ($propertiesUsedOn as $rootPageId => $pages) {
+                    $messageBody .= $pages['rootPageData']['title'] . ' [' . $pages['rootPageData']['uid'] . '] => ';
+
+                    $messageBodyData = [];
+                    foreach ($pages['pages'] as $page) {
+                        $messageBodyData[] = $page['title'] . ' [' . $page['uid'] . ']';
+                    }
+                    $messageBody .= implode(', ', $messageBodyData);
+
+                    $messageBody .= ' // ';
                 }
-
-                $messageBody = implode(', ', $messageBodyData);
 
                 $pageLayoutController->moduleTemplate->addFlashMessage(
                     $messageBody,
