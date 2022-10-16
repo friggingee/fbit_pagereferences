98499060169589c6c69f355889d5b85122284870
Grigori Prokhorov <hello@feinberg.it>
2022-10-16 16:54:51 +0200

    [WIP] fix field availability tests

--- Classes/Hooks/Core/DataHandling/DataHandler/ProcessCmdmapClass/DontHideRecordsWhenConvertingContentReferencesToCopies.php
+++ Classes/Hooks/Core/DataHandling/DataHandler/ProcessCmdmapClass/DontHideRecordsWhenConvertingContentReferencesToCopies.php
@@ -15,10 +15,13 @@ class DontHideRecordsWhenConvertingContentReferencesToCopies
     {
         if (isset($dataHandler->cmdmap['tt_content'])
             && is_array($dataHandler->cmdmap['tt_content'])
-            && isset($dataHandler->cmdmap['tt_content']['copy'])
+
+            && isset(reset($dataHandler->cmdmap['tt_content'])['copy'])
             && is_array(reset($dataHandler->cmdmap['tt_content'])['copy'])
-            && isset($dataHandler->cmdmap['tt_content']['copy']['parentAction'])
+
+            && isset(reset($dataHandler->cmdmap['tt_content'])['copy']['parentAction'])
             && !empty(reset($dataHandler->cmdmap['tt_content'])['copy']['parentAction'])
+
             && reset($dataHandler->cmdmap['tt_content'])['copy']['parentAction'] === 'convertReferencesToCopies'
         ) {
             $dataHandler->neverHideAtCopy = true;
